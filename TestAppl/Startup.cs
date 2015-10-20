9a638f6eeb7f4cf3422846cf26983967ebd5735a
Dimitris Machairas <dim_maxairas@yahoo.gr>
2015-10-20 22:17:38 +0300

    Initial Commit

---
+++ TestAppl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAppl.Startup))]
+namespace TestAppl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
